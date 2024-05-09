21bb78c783341467ff16b01701b7670c86f6b260
Brad Siegel <blsiege@gmail.com>
2024-05-09 12:08:46 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Brad Siegel on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
