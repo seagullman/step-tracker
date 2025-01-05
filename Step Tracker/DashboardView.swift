062a1e548346b036ed964dd220afb39c5f40995f
Brad Siegel <blsiege@gmail.com>
2025-01-04 19:27:03 -0500

    data list view initial ui

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -22,7 +22,7 @@ enum HealthMetricContext: CaseIterable, Identifiable {
     }
 }
 
-struct ContentView: View {
+struct DashboardView: View {
     
     @State private var selectedStat: HealthMetricContext = .steps
     
@@ -91,7 +91,7 @@ struct ContentView: View {
             .padding()
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
-                Text(metric.title)
+                HealthDataListView(metric: metric)
             }
         }
         .tint(isSteps ? .pink : .indigo)
@@ -99,5 +99,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    DashboardView()
 }
